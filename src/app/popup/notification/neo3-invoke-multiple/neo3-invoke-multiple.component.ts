6d9078f3ffc6fa60fcb8d6a67078e20557c196c1
bao <965001935@qq.com>
2021-07-02 18:51:07 +0800

    fix: N3 dapi invoke multiple method

--- src/app/popup/notification/neo3-invoke-multiple/neo3-invoke-multiple.component.ts
+++ src/app/popup/notification/neo3-invoke-multiple/neo3-invoke-multiple.component.ts
@@ -74,7 +74,6 @@ export class PopupNoticeNeo3InvokeMultipleComponent implements OnInit {
                     hostname: undefined,
                 };
                 this.pramsData = params;
-                this.invokeArgs = this.pramsData.invokeArgs;
                 this.pramsData.invokeArgs.forEach(item => {
                     item = this.neo3Invoke.createInvokeInputs(item);
                     this.invokeArgs.push({
